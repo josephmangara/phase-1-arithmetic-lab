0b88c41c0778ae93080281683e9a7005e52d13fe
josephmangara <josephmangara2@gmail.com>
2023-10-06 10:41:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+    const num1 = 31;
+    const num2 = 2;
+    const multiply = num1 * num2
+
+
+   const random = Math.floor(Math.random() * 99) + 1;
+    
+const num3 = 39;
+const num4 = 7;
+const mod = 39 % 7;
+
+let max = Math.max(13, 8, 20, 11, 2);
